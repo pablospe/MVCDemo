7ec371ce1d8a246bc05f450520301967b0de58c2
Pablo Speciale <pablo.speciale@gmail.com>
2014-01-14 23:42:50 -0300

    Giving sintax format to SamplingFS.glsl

--- src/SamplingFS.glsl
+++ src/SamplingFS.glsl
@@ -4,15 +4,15 @@ varying vec4 membrane;
 
 uniform int mode;
 
-void main(){
+void main()
+{
+  vec4 target = texture2DRect(tex0, gl_TexCoord[0].st);
+  vec4 source = texture2DRect(tex1, gl_TexCoord[1].st);
 
-vec4 target = texture2DRect(tex0, gl_TexCoord[0].st);
-vec4 source = texture2DRect(tex1, gl_TexCoord[1].st);
-
-if (mode==1)
-gl_FragColor = source;
-else if(mode==2)
-gl_FragColor = membrane + 0.5;
-else
-gl_FragColor = membrane+source;
+  if (mode==1)
+    gl_FragColor = source;
+  else if(mode==2)
+    gl_FragColor = membrane + 0.5;
+  else
+    gl_FragColor = membrane+source;
 }
